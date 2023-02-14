cdae318acdb117b8b74ac593e60e643d1c066e07
bitue <ashikul.islam_ashik@yahoo.com>
2023-02-14 07:03:57 +0600

    trim

--- model/blogModel.js
+++ model/blogModel.js
@@ -4,7 +4,7 @@ const blogSchema = new mongoose.Schema(
     {
         title: {
             type: String,
-
+            trim: true,
             min: [100, 'title must be 100 characters!'],
             required: [true, 'title is required ']
         },
