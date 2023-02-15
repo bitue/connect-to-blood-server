327013008c0dbcf9500ca2966b69f9e181c5c3cd
bitue <ashikul.islam_ashik@yahoo.com>
2023-02-15 13:27:00 +0600

    mongo set

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config(); // for access the dot env vars
 
 const connectDB = async () => {
+    mongoose.set('strictQuery', false);
     try {
         await mongoose.connect(process.env.DB_CONNECTION_STRING, () => {
             console.log('DB connected successfully ');
