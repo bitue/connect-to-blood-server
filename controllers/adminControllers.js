13dbb4f6e36e93d341fb84e5870e0b078b898871
bitue <ashikul.islam_ashik@yahoo.com>
2023-02-16 10:55:55 +0600

    add admin api for dashboard

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,8 +1,63 @@
-const makeAdmin = (req, res, next) => {
-    // done to do admin work
-    res.send('ok');
+const { Blog } = require('../model/blogModel');
+const { User } = require('../model/userModel');
+
+const makeAdmin = async (req, res, next) => {
+    try {
+        const resToDb = await User.updateOne({ _id: req.query.id }, { $set: { role: 'admin' } });
+        res.json(resToDb);
+    } catch (err) {
+        next(err);
+    }
+};
+const getAllUsers = async (req, res, next) => {
+    try {
+        const allUsers = await User.find({ role: 'user' }, {}, { sort: { createdAt: -1 } });
+        res.json({
+            data: allUsers,
+            status: 'ok'
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const getUserById = async (req, res, next) => {
+    try {
+        const userDB = await User.findById(req.query.id);
+        res.json({
+            data: userDB,
+            status: 'ok'
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const banUserById = async (req, res, next) => {
+    try {
+        const userDeleteRes = await User.deleteOne({ _id: req.query.id });
+        res.json(userDeleteRes);
+    } catch (err) {
+        next(err);
+    }
+};
+
+const getBlogsByUserId = async (req, res, next) => {
+    try {
+        const userDB = await Blog.find({ user: req.query.id });
+        res.json({
+            data: userDB,
+            status: 'ok'
+        });
+    } catch (err) {
+        next(err);
+    }
 };
 
 module.exports = {
-    makeAdmin
+    makeAdmin,
+    getAllUsers,
+    getUserById,
+    banUserById,
+    getBlogsByUserId
 };
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const { checkAuth } = require('../authentication/authorization');
+const {
+    getAllUsers,
+    getUserById,
+    banUserById,
+    getBlogsByUserId,
+    makeAdmin
+} = require('../controllers/adminControllers');
 const { checkRole } = require('../middlewares/checkRole');
 const adminRouter = express.Router();
 
-adminRouter.get('/makeAdmin', checkAuth, checkRole('user'), (req, res) => {
-    console.log(1);
-    res.sendStatus(200);
-});
-
+// admin can make another admin
+// adminRouter.get('/makeAdmin', checkAuth, checkRole('admin'), (req, res) => {
+//     console.log(1);
+//     res.sendStatus(200);
+// });
+// admin can get all users
+adminRouter.get('/getAllUsers', checkAuth, checkRole('admin'), getAllUsers);
+// admin can see a user by id
+adminRouter.get('/getUserById', checkAuth, checkRole('admin'), getUserById);
+//admin can ban a user or donor by id
+adminRouter.delete('/banUserById', checkAuth, checkRole('admin'), banUserById);
+// admin can get  blogs by userid
+adminRouter.get('/getBlogsByUserId', checkAuth, checkRole('admin'), getBlogsByUserId);
+// admin can make another user or donor to admin
+adminRouter.put('/makeAdmin', checkAuth, checkRole('admin'), makeAdmin);
 module.exports = {
     adminRouter
 };
