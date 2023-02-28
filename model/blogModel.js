0e3ba776423bba3ca85b896f76bc0d1636716db0
bitue <ashikul.islam_ashik@yahoo.com>
2023-03-01 03:10:20 +0600

    change the like model schema

--- model/blogModel.js
+++ model/blogModel.js
@@ -28,11 +28,12 @@ const blogSchema = new mongoose.Schema(
                 ref: 'Comment'
             }
         ],
-        likes: {
-            type: Number,
-            default: 0,
-            required: true
-        },
+        likes: [
+            {
+                type: mongoose.SchemaTypes.ObjectId,
+                ref: 'User'
+            }
+        ],
         disLikes: {
             type: Number,
             default: 0,
